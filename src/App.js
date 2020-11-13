a16e3212c4cc0f208f70656a5a0e84251eb4a004
Cbeterous <celine.beterous@gmail.com>
2020-11-13 10:49:31 +0100

    contactlist
    quete3

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
-import { Contact } from './components/Contact';
+import {ContactList} from './components/ContactList';
+
+
 
 function App() {
   return (
     <div className="App">
-      <Contact online={true} name='Jessie Alexander' avatar='https://randomuser.me/api/portraits/men/0.jpg'></Contact>
-      <Contact online={false} name='Lynn Stevens' avatar='https://randomuser.me/api/portraits/women/81.jpg'></Contact>
-      <Contact online={false} name='Jim Sutton' avatar='https://randomuser.me/api/portraits/men/48.jpg'></Contact>
+        <ContactList />
     </div>
   );
 }
